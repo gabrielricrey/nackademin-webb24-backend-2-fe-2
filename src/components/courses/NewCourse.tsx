56a08856888037996848eb53525bcf8b2a9db090
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-10-16 09:52:20 +0200

    add course service for handling course API

--- src/components/courses/NewCourse.tsx
+++ src/components/courses/NewCourse.tsx
@@ -1,11 +1,25 @@
 "use client"
 
 import CourseForm from "@/components/courses/CourseForm";
+import CourseService from "@/utils/courseService";
+import { useRouter } from "next/navigation";
 
 export default function NewCourse() {
+  const router = useRouter()
 
-  const onCreate = (courseData: Partial<Course>) => {
+  const onCreate = async (courseData: Partial<Course>) => {
     console.log("CREATE NEW COURSE", courseData)
+    try {
+      const response = await new CourseService().createCourse(courseData)
+      if(!response.ok){
+        throw new Error("Course could not be created")
+      }
+      const newCourse: Course = await response.json()
+      router.push(`/courses/${newCourse.course_id}/update/`)
+
+    } catch (err) {
+      console.warn("Error in creating course", err)
+    }
   }
 
   return (
